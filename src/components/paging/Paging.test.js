647f978a37fbae40da6492cd9ea6438c7230b84e
Emily Creamer <emily.creamer@icloud.com>
2019-05-19 11:32:38 -0700

    Paging tests passing

--- src/components/paging/Paging.test.js
+++ src/components/paging/Paging.test.js
@@ -23,5 +23,38 @@ describe('Paging Component', () => {
     expect(next).toHaveBeenCalledWith(3);
   });
 
-  
+  it('Decrements page on Previous Button click', () => {
+    const next = jest.fn();
+    const wrapper = shallow(
+      <Paging currentPage={2} totalPages={10} nextPage={next}>
+        <h1>Hello.</h1>
+      </Paging>
+    );
+    wrapper.find('button').at(0).simulate('click');
+    expect(next).toHaveBeenCalledWith(1);
+  });
+
+  it('Does not display the previous button when on page 01', () => {
+    const next = jest.fn();
+    const wrapper = shallow(
+      <Paging currentPage={1} totalPages={10} nextPage={next}>
+        <h1>Hello...</h1>
+      </Paging>
+    );
+    const buttons = wrapper.find('button');
+    expect(buttons).toHaveLength(1);
+    expect(buttons.at(0).text()).toEqual('Next Page');
+  });
+
+  it('Does not display the next button when on last page', () => {
+    const next = jest.fn();
+    const wrapper = shallow(
+      <Paging currentPage={10} totalPages={10} nextPage={next}>
+        <h1>Hello.</h1>
+      </Paging>
+    );
+    const buttons = wrapper.find('button');
+    expect(buttons).toHaveLength(1);
+    expect(buttons.at(0).text()).toEqual('Previous Page');
+  });
 });
